a21e8f71801d5c6546b9c268a428e6aab239aefd
Heather Ward <heather.ward13@gmail.com>
2025-01-15 21:57:07 -0500

    Add styles and nginx conf

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-local:
-	docker run --rm -it --network host -v $$PWD/src:/usr/share/nginx/html nginx:latest
+local: build
+	docker run --rm -it --network host -v $$PWD/src:/usr/share/nginx/html hkeward/website:latest
+
+build:
+	docker build --rm -t hkeward/website:latest .
---
+++ src/js/navbar.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarPlaceholder = document.getElementById('navbar');
+  if (navbarPlaceholder) {
+    fetch('/html/navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        navbarPlaceholder.innerHTML = data;
+      })
+      .catch(err => console.error('Failed to load navbar:', err));
+  }
+});
